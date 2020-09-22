a6476d10ac042c1195060629640fe5c3c0860959
Pengjie Zhou <zhoupj1215@hotmail.com>
2020-09-22 10:50:39 -0700

    set backlog task relation

--- PPMToolFullStack/src/main/java/com/example/ppmtool/domain/Project.java
+++ PPMToolFullStack/src/main/java/com/example/ppmtool/domain/Project.java
@@ -13,40 +13,27 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @NotBlank(message = "Project name is required")
     private String projectName;
-
     @NotBlank(message ="Project Identifier is required")
-    @Size(min=4, max=6, message = "Please use 4 to 6 characters")
+    @Size(min=4, max=5, message = "Please use 4 to 5 characters")
     @Column(updatable = false, unique = true)
     private String projectIdentifier;
-
     @NotBlank(message = "Project description is required")
     private String description;
-
     @JsonFormat(pattern = "yyyy-mm-dd")
     private Date start_date;
-
     @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
-
     @JsonFormat(pattern = "yyyy-mm-dd")
     @Column(updatable = false)
     private Date created_At;
-
     @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_At;
 
-    @PrePersist
-    protected void onCreate() {
-        this.created_At = new Date();
-    }
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "project")
 
-    @PreUpdate
-    protected void onUpdate() {
-        this.updated_At = new Date();
-    }
+    private Backlog backlog;
 
     public Project() {
     }
@@ -115,5 +102,22 @@ public class Project {
         this.updated_At = updated_At;
     }
 
+    public Backlog getBacklog() {
+        return backlog;
+    }
+
+    public void setBacklog(Backlog backlog) {
+        this.backlog = backlog;
+    }
+
+    @PrePersist
+    protected void onCreate(){
+        this.created_At = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updated_At = new Date();
+    }
 
 }
