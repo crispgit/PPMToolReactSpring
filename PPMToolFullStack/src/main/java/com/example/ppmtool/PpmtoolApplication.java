8909954c8bce1d67b02dc49be3c92b9c591d3e9d
Pengjie Zhou <zhoupj1215@hotmail.com>
2020-09-24 12:24:59 -0700

    add test and deploy

--- PPMToolFullStack/src/main/java/com/example/ppmtool/PpmtoolApplication.java
+++ PPMToolFullStack/src/main/java/com/example/ppmtool/PpmtoolApplication.java
@@ -2,15 +2,22 @@ package com.example.ppmtool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class PpmtoolApplication {
+public class PpmtoolApplication extends SpringBootServletInitializer {
 
     @Bean
     BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PpmtoolApplication.class);
     }
 
     public static void main(String[] args) {
